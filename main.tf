c987673087ada48691275dbefbc3e83d0e5ef4cb
Sudhamsh <ramasudhamsh@gmail.com>
2025-06-06 19:48:15 -0400

    s3 commit

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 provider "aws" {
   region = "us-east-1"
 }
-adding another commit
-adding one more commit
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "sudhamsh-devops-bucket-12345" # must be globally unique
+  acl    = "private"
+}
 
